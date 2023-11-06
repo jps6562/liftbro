005300732604f9ede65a285c2879831a20cc3d6a
jps6562 <jselnekovicjr@gmail.com>
2023-11-05 23:49:45 -0500

    started tic tac toe tutorial

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import './App.css';
+import { useState } from 'react';
 
-
-const App = () => {
+export default function Board() {
   return (
-    <h1>Hello World!</h1>
+    <>
+      <div className="board-row">
+        <Square />
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+    </>
   );
-};
+}
 
-export default App;
+function Square() {
+  const [value, setValue] = useState(null);
 
-
-
+  function handleClick()
+  {
+    setValue('X');
+    console.log('clicked');
+  }
+  return <button className="square" onClick={handleClick}>{value}</button>;
+}
