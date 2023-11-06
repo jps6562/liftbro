955a91dba0d21f4078fc0c2968b7990e2449c208
jps6562 <jselnekovicjr@gmail.com>
2023-11-05 23:21:41 -0500

    Basic Hello World

--- src/App.js
+++ src/App.js
@@ -1,37 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
-export default function App() {
+
+const App = () => {
   return (
-    <div>
-      <h1>Welcome to my app</h1>
-      <MyButton />
-    </div>
+    <h1>Hello World!</h1>
   );
-}
+};
 
-class APP2 extends App{
-  state = {
-    showMessage: false
-  }
-  onButtonClickHandler = () => {
-   this.setState({showMessage: true});
-  };
-
-  render(){ 
-    return(<div className="App">
-     {this.state.showMessage && <p>Hi</p>}
-      <button onClick={this.onButtonClickHandler}>Enter</button>
-    </div>);
-
-  }
-}
-
-function MyButton() {
-  return (
-    <button>Press Me</button>
-  );
-}
+export default App;
 
 
 
